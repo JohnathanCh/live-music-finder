7407e75b0b2bd62dbf3a4489ee5a9311cc6631bc
github username <github email address>
2018-05-30 10:19:15 -0400

    Added to  migrations

--- db/migrate/20180530135037_create_performances.rb
+++ db/migrate/20180530135037_create_performances.rb
@@ -1,6 +1,8 @@
 class CreatePerformances < ActiveRecord::Migration[5.2]
   def change
     create_table :performances do |t|
+      t.belongs_to :event
+      t.belongs_to :artist
 
       t.timestamps
     end
